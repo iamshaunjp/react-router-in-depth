0c0112bc75ca517477252017893959c006ef74a2
Shaun <shaun@thenetninja.co.uk>
2022-12-02 16:41:42 +0000

    lesson-11

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,21 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+
 export default function About() {
+  const [user, setUser] = useState('mario')
+  
+  if (!user) {
+    return <Navigate to="/" replace={true} />
+  }
+
   return (
     <div className="about">
       <h2>About Us</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
+
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   )
 }
