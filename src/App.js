f26d187735a107e62d7eddbb0b74ef627fd5e0a9
Shaun <shaun@thenetninja.co.uk>
2022-12-02 12:21:25 +0000

    lesson-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
