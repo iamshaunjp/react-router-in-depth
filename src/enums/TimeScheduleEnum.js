ed2eb5ac1c87d775a0fea0d2380e9e16d1c7012d
Konstantinos Razgkel <k.razgkelis@outlook.com>
2023-11-02 12:32:04 +0200

    Add timeschedile enums

---
+++ src/enums/TimeScheduleEnum.js
@@ -0,0 +1,21 @@
+class HoursEnum {
+
+    static HOUR_8 = 8;
+    static HOUR_9 = 9;
+    static HOUR_10 = 10;
+    static HOUR_11 = 11;
+    static HOUR_12 = 12;
+    static HOUR_13 = 13;
+    static HOUR_14 = 14;
+    static HOUR_15 = 15;
+    static HOUR_16 = 16;
+    static HOUR_17 = 17;
+    static HOUR_18 = 18;
+    static HOUR_19 = 19;
+    static HOUR_20 = 20;
+    static HOUR_21 = 21;
+    static HOUR_22 = 22;
+    static HOUR_23 = 23;
+  };
+
+  export default HoursEnum;
